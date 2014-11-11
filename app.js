ef1070c1fb088ba1cd6bfd7a6db402b3272f2dd6
Andrei Shchatko <andrei.shchatko@gmail.com>
2014-11-11 23:51:05 +0300

    Init commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var ejs = require('ejs');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.engine('html', ejs.renderFile);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Port: ' + port);
+});
