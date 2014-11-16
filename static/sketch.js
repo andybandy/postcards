6f502914381126450d7b596278eb22b20a12c51d
Andrei Shchatko <andrei.shchatko@gmail.com>
2014-11-16 20:48:41 +0300

    Small changes to code to simplify reading

--- static/sketch.js
+++ static/sketch.js
@@ -2,28 +2,27 @@
   var app = {};
 
   app.init = function(imgPath) {
-    app.s = function(sketch) {
+    app.s = function(s) {
       var img, cSlider;
 
-      sketch.preload = function() {
-        img = sketch.loadImage(imgPath);
+      s.preload = function() {
+        img = s.loadImage(imgPath);
       };
 
-      sketch.setup = function() {
-        sketch.createCanvas(640, 480);
-        sketch.background(200);
-        sketch.image(img, 0, 0, sketch.width, sketch.height);
-        cSlider = sketch.createSlider(0, 1, 0.5);
+      s.setup = function() {
+        s.createCanvas(640, 480);
+        s.background(200);
+        s.image(img, 0, 0, s.width, s.height);
+        cSlider = s.createSlider(0, 1, 0.5);
         cSlider.position(20, 20);
       };
 
-      sketch.draw = function() {
+      s.draw = function() {
         var opaqueLevel = cSlider.value();
-        console.log(opaqueLevel);
-        sketch.filter('opaque', opaqueLevel);
+        s.filter('opaque', opaqueLevel);
       };
 
-      return sketch;
+      return s;
     };
 
     app.p = new p5(app.s);
